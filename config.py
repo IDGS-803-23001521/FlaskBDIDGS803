a9f59f46babeea50a94ae3482528e30e53566446
IDGS-803-23001521 <marianacortes.escolar@gmail.com>
2026-02-12 21:36:41 -0600

    12-02-2026 Archivo de configuracion

---
+++ config.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+
+class Config(object):
+    SECRET_KEY="ClaveSecreta"
+    SESSION_COOKIE_SECURE=False
+
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://cardiel:root@127.0.0.1/bdidgs803'
+    SQLALCHEMY_TRACK_MODIFICATIONS=false
